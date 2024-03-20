43fc6709c8aa305875b2bea051d35dfa5a86a129
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-03-20 20:13:40 +0000

    feat: home page style and navbar

--- configs/hbs.config.js
+++ configs/hbs.config.js
@@ -5,17 +5,17 @@ const daysJs = require("./dayjs.config");
 
 
 hbs.registerPartials(`${__dirname}/../views/partials`);
-hbs.registerHelper("categoryImage", function(category) {
-    
-    switch(category) {
+hbs.registerHelper("categoryImage", function (category) {
+
+    switch (category) {
         case "Crafts":
             return "/img/icons/crafts.png";
-        case  "Cooking":
+        case "Cooking":
             return "/img/icons/cooking.png";
         case "Gardening and Horticulture":
             return "/img/icons/Gardening and Horticulture.png";
-        case "Everyday Life skills" :
-            return  "/img/icons/EverydayLifeSkills.png";
+        case "Everyday Life skills":
+            return "/img/icons/EverydayLifeSkills.png";
         case "Music":
             return "/img/icons/music.png";
         case "Sports":
@@ -25,35 +25,35 @@ hbs.registerHelper("categoryImage", function(category) {
         case "Languages and Culture":
             return "/img/icons/Languages and Culture";
         case "Others":
-            return "/img/icons/others.png"        
+            return "/img/icons/others.png"
     }
 })
 
 hbs.registerHelper('ifEq', function (category1, category2, options) {
     if (category1 === category2) {
-      return options.fn(this);
+        return options.fn(this);
     } else {
-      return options.inverse(this);
+        return options.inverse(this);
     }
-  })
+})
 hbs.registerHelper("dateFormat", function (options) {
-const { date, format } = options.hash;
-return daysJs(date).format(format || "YYYY-MM-DD HH:mm:ss")
+    const { date, format } = options.hash;
+    return daysJs(date).format(format || "YYYY-MM-DD HH:mm:ss")
 
 })
 
 
 
 
-hbs.registerHelper("ifRequesterIsLogged",function (requester, userLogged, options){
+hbs.registerHelper("ifRequesterIsLogged", function (requester, userLogged, options) {
 
-    if (requester == userLogged){
-         return options.fn(this);
+    if (requester == userLogged) {
+        return options.fn(this);
     } else {
         return options.inverse(this)
     }
 })
-hbs.registerHelper("ifUserRated", function(skillsRatings, currentUser, options) {
+hbs.registerHelper("ifUserRated", function (skillsRatings, currentUser, options) {
     if (!skillsRatings) {
         return options.fn(this)
     }
--- controllers/petition.controllers.js
+++ controllers/petition.controllers.js
@@ -6,36 +6,37 @@ const mongoose = require("mongoose");
 module.exports.show = (req, res, next) => {
     const username = req.user.username
 
-    Petition.find() 
+    Petition.find()
         .populate("requester")
-        .then ((petitions) => {
-            
-             res.render("petitions/show", { petitions, username   });
-             console.debug(`esto es petetions.requester ${petitions[0].requester}`)
-             console.debug(`esto es username ${username}`)
+        .then((petitions) => {
 
-             
+            res.render("petitions/show", { petitions, username });
+            console.debug(`esto es petetions.requester ${petitions[0].requester}`)
+            console.debug(`esto es username ${username}`)
+
+
         })
         .catch((error) => next(error));
-    
-   ;
+
+    ;
 }
 module.exports.doCreate = (req, res, next) => {
-   const petition = req.body;
-   petition.name = req.body.name;
-   petition.description = req.body.description;
-   petition.requester = req.user.id;
-   petition.category = req.body.category;
-   
+    const petition = req.body;
+    petition.name = req.body.name;
+    petition.description = req.body.description;
+    petition.requester = req.user.id;
+    petition.category = req.body.category;
 
-   Petition.create(petition)
-    .then((petitionCreated) => res.redirect("/petitions/show"))
-    .catch((error) => next (error))
+
+    Petition.create(petition)
+        .then((petitionCreated) => res.redirect("/petitions/show"))
+        .catch((error) => next(error))
 }
 module.exports.delete = (req, res, next) => {
     const { id } = req.params;
     Petition.findByIdAndDelete(id)
-    .then(() => {
-        res.redirect("/petitions/show")})
-    .catch((error) => next(error))
+        .then(() => {
+            res.redirect("/petitions/show")
+        })
+        .catch((error) => next(error))
 }
